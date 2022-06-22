6540b20fbc7bbcf330a5ac350fcc56f1a13ee05a
Boubacar Sow <boubasow.pro@gmail.com>
2022-06-21 23:44:12 -0400

    Adding methods to retreive employees to the EmployeeService interface

--- springboot-project/src/main/java/com/springboot/springbootproject/service/EmployeeService.java
+++ springboot-project/src/main/java/com/springboot/springbootproject/service/EmployeeService.java
@@ -1,7 +1,11 @@
 package com.springboot.springbootproject.service;
 
+import java.util.List;
+
 import com.springboot.springbootproject.model.*;
 
 public interface EmployeeService {
 	Employee saveEmployee(Employee employee);
+	List<Employee> getAllEmployees();
+	Employee getEmployeeById(long id);
 }
